005ffd50b3fe6bf8d0a1ab15950fdd103ca604d9
Grekos89 <70657407+Grekos89@users.noreply.github.com>
2023-01-21 16:19:55 +0300

    задача1

---
+++ number1/Program.cs
@@ -0,0 +1,25 @@
+﻿// Напишите программу которая по заданной четверти
+//  показывает диапазон возможных координат точек этой четврти (x и y).
+int number  = new Random().Next(1,5);
+Console.WriteLine(number);
+
+if(number == 1)
+{
+    Console.WriteLine("x = (0, + бесконечность) y = (0, +бесконечность)");  
+}
+else if(number == 2)
+{
+    Console.WriteLine("x = (0, - бесконечность) y = (0, +бесконечность)");
+}
+else if(number == 3)
+{
+    Console.WriteLine("x = (0, - бесконечность) y = (0, -бесконечность)");
+}
+else if(number == 4)
+{
+    Console.WriteLine("x = (0, + бесконечность) y = (0, +бесконечность)");
+}
+else if(number < 0 || number > 4)
+{
+    Console.WriteLine("нет такой четверти");
+}
