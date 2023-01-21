e5c4d26f815e4c8fcdb5a6701f2b3fece9bb4811
Grekos89 <70657407+Grekos89@users.noreply.github.com>
2023-01-21 19:15:43 +0300

    задача3

---
+++ number3/Program.cs
@@ -0,0 +1,9 @@
+﻿//Напишите програму которая принемает на вход число (N) и выдаёт таблицу
+//квадратов чисел от 1 до N
+int number = new Random().Next(1, 20);
+System.Console.WriteLine($"number = {number}");
+
+for(int i = 1; i <= number; i++)
+{
+    System.Console.WriteLine(Math.Pow(i, 2));
+}
